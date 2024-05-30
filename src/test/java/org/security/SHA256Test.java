f8d7234339cd91be134c3762b79b176dd53d05a8
tadeas223 <maca.tadeas@gmail.com>
2024-05-30 09:57:53 +0200

    SHA256 test added and passed

---
+++ src/test/java/org/security/SHA256Test.java
@@ -0,0 +1,23 @@
+package org.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SHA256Test {
+
+    @Test
+    void encode() {
+        String text1 = "hello";
+        String text2 = "hello2";
+
+        String good1 = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+        String good2 = "87298cc2f31fba73181ea2a9e6ef10dce21ed95e98bdac9c4e1504ea16f486e4";
+
+        String result1 = SHA256.encode(text1);
+        String result2 = SHA256.encode(text2);
+
+        assertEquals(good1,result1);
+        assertEquals(good2,result2);
+    }
+}
